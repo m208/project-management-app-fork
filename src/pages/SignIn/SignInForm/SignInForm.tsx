41f05360053779c21a742da460d309098a8aa3d5
m208 <avcvet@yandex.ru>
2022-11-14 17:09:16 +0300

    feat: add Spinner component

--- src/pages/SignIn/SignInForm/SignInForm.tsx
+++ src/pages/SignIn/SignInForm/SignInForm.tsx
@@ -5,12 +5,13 @@ import { useEffect } from 'react';
 
 import { saveLocalAuthState } from '@/app/auth';
 import { IUserSignInData } from '@/app/types';
+import { Loader } from '@/components/Loader/Loader';
 import { useAppDispatch, useAppSelector } from '@/hooks/redux';
 import './SignInForm.pcss';
 import { userLogIn } from '@/store/reducers/AuthSlice';
 
 export const SignInForm = (): JSX.Element => {
-  const { isLoggedIn, user, token } = useAppSelector(
+  const { isLoggedIn, user, token, awaiting } = useAppSelector(
     state => state.authReducer,
   );
   const dispatch = useAppDispatch();
@@ -19,7 +20,6 @@ export const SignInForm = (): JSX.Element => {
   const { register, handleSubmit, formState: { errors } } = useForm();
 
   const onSubmit = async (data: Record<string, string>) => {
-
     const userData: IUserSignInData = {
       login: data.login,
       password: data.password,
@@ -36,53 +36,55 @@ export const SignInForm = (): JSX.Element => {
   }, [isLoggedIn]);
 
   return (
-    <div className='signin-form-wrapper'>
-      <form className="form" onSubmit={(handleSubmit(onSubmit))}>
+    <>
+      {(awaiting && <Loader/> )}
 
-        <div className="form-item">
-          <label htmlFor='signin-login' className="form-item-label">
+      <div className='signin-form-wrapper'>
+        <form className="form" onSubmit={(handleSubmit(onSubmit))}>
+
+          <div className="form-item">
+            <label htmlFor='signin-login' className="form-item-label">
              Login:
+              <input
+                id='signin-login'
+                className='signin-input'
+                {...register('login', { required: true })}
+                aria-invalid={errors.login ? 'true' : 'false'}
+                type="text"
+              />
+            </label>
 
-            <input
-              id='signin-login'
-              className='signin-input'
-              {...register('login', { required: true })}
-              aria-invalid={errors.login ? 'true' : 'false'}
-              type="text"
-            />
-          </label>
-
-          <div className="error-field">
-            {errors.password?.type === 'required' && (
-              <span role="alert">Enter login</span>
-            )}
+            <div className="error-field">
+              {errors.password?.type === 'required' && (
+                <span role="alert">Enter login</span>
+              )}
+            </div>
           </div>
-        </div>
 
-        <div className="form-item">
-          <label htmlFor='signin-password' className="form-item-label">
+          <div className="form-item">
+            <label htmlFor='signin-password' className="form-item-label">
              Password:
-
-            <input
-              id='signin-password'
-              className='signin-input'
-              {...register('password', { required: true })}
-              aria-invalid={errors.password ? 'true' : 'false'}
-              type="text"
-            />
-          </label>
-          <div className="error-field">
-            {errors.password?.type === 'required' && (
-              <span role="alert">Enter password</span>
-            )}
+              <input
+                id='signin-password'
+                className='signin-input'
+                {...register('password', { required: true })}
+                aria-invalid={errors.password ? 'true' : 'false'}
+                type="text"
+              />
+            </label>
+            <div className="error-field">
+              {errors.password?.type === 'required' && (
+                <span role="alert">Enter password</span>
+              )}
+            </div>
           </div>
-        </div>
 
-        <div className="form-submit-wrapper">
-          <input type="submit" className='submit-button' value="Sign in!"/>
-        </div>
-      </form>
-    </div>
+          <div className="form-submit-wrapper">
+            <input type="submit" className='submit-button' value="Sign in!"/>
+          </div>
+        </form>
+      </div>
+    </>
   );
 
 };
