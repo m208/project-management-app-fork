9c2816c6c7c4edfb81137cf9eb5b085076c0c813
m208 <avcvet@yandex.ru>
2022-11-15 20:36:03 +0300

    fix: math error

--- src/app/constants.ts
+++ src/app/constants.ts
@@ -1,3 +1,3 @@
 export const API_ENDPOINT = 'https://rss-kanban-backend-production.up.railway.app';
 
-export const tokenExpirationValue = 4 * 60 * 3600;  // 4 hours in ms
+export const tokenExpirationValue = 4 * 60 * 60 * 1000;  // 4 hours in ms
