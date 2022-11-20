2cbae6baa6dd55bd391ff40e8114cd43bb03f058
Татьяна Русак <tatianarusak85@gmail.com>
2022-11-21 02:00:09 +0300

    feat: add scrollbars for columns

--- src/pages/Board/Board.tsx
+++ src/pages/Board/Board.tsx
@@ -13,38 +13,38 @@ export const Board = (): JSX.Element => {
 
   return (
     <section className="board">
-      <div className="container">
-        {(isLoading && <Loader/> )}
+      {(isLoading && <Loader/> )}
 
-        {error && (
-          <>
-            <p>Board does not exist</p>
-            <Link to='/boards'>
-                Go back to yours boards
-            </Link>
-          </>
-        )}
+      {error && (
+        <>
+          <p>Board does not exist</p>
+          <Link to='/boards'>
+              Go back to yours boards
+          </Link>
+        </>
+      )}
 
-        {board && (
-          <>
-            <div className="board-heading">
-              <span className='board-crumbs'>
-                <Link to='/boards'>
-                  Back to boards
-                </Link>
+      {board && (
+        <>
+          <div className="board-heading">
+            <span className='board-crumbs'>
+              <Link to='/boards'>
+                Back to boards
+              </Link>
 
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20" strokeWidth={2} stroke="currentColor" className="w-4 h-4 mx-2">
-                  <path strokeLinecap="round" strokeLinejoin="round" d="M18.75 19.5l-7.5-7.5 7.5-7.5m-6 15L5.25 12l7.5-7.5" />
-                </svg>
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20" strokeWidth={2} stroke="currentColor" className="w-4 h-4 mx-2">
+                <path strokeLinecap="round" strokeLinejoin="round" d="M18.75 19.5l-7.5-7.5 7.5-7.5m-6 15L5.25 12l7.5-7.5" />
+              </svg>
 
-              </span>
-              <h1 className = 'board-title'>{board.title}</h1>
-            </div>
+            </span>
+            <h1 className = 'board-title'>{board.title}</h1>
+          </div>
 
+          <div className="board-wrapper">
             <ColumnsContainer boardId={board.id} />
-          </>
-        )}
-      </div>
+          </div>
+        </>
+      )}
     </section>
   );
 };
