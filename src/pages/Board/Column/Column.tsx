c7baea67edd095ce1dba4dbd1e4f9c0e2bb56eb4
Tatiana Rusak <tatianarusak85@gmail.com>
2022-11-27 01:47:34 +0300

    fix: change icon stroke

--- src/pages/Board/Column/Column.tsx
+++ src/pages/Board/Column/Column.tsx
@@ -105,12 +105,12 @@ export const Column = ({ boardId, column, onDelete }: ColumnProps): JSX.Element 
   };
 
   const saveButtonLabel = (
-    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={2} stroke="green" className="w-6 h-6">
       <path strokeLinecap="round" strokeLinejoin="round" d="M4.5 12.75l6 6 9-13.5" />
     </svg>
   );
   const cancelButtonLabel = (
-    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={2} stroke="#C2412B" className="w-6 h-6">
       <path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12" />
     </svg>
   );
